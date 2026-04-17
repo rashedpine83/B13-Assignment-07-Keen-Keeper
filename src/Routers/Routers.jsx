12dfc92915f6cedb86f1e723e5f9fc3b364dbbff
rashedpine83 <rashedpine83@gmail.com>
2026-04-17 10:13:27 +0600

    Created Error Page

--- src/Routers/Routers.jsx
+++ src/Routers/Routers.jsx
@@ -5,6 +5,7 @@ import Timeline from "../Components/Timeline/Timeline";
 import AllFriends from "../Components/HomePage/AllFriends";
 import FriendDetails from "../Pages/FriendDetails";
 import Dashboard from "../Components/Status/Status";
+import ErrorPage from "../Pages/ErrorPage/ErrorPage";
 
 
 export const router = createBrowserRouter([
@@ -29,10 +30,9 @@ export const router = createBrowserRouter([
         path: '/status',
         element: <Dashboard></Dashboard>
       }
-    ]
-  },
-
-  {
-    
+    ],
+     errorElement: <ErrorPage></ErrorPage>
   }
+
+  
 ]);
